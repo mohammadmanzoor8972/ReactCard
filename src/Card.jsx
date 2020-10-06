fcbc5da5de78b0ddda81e6ef6a91f1ad207a3257
Mohammad Manzoor Alam <mohammad.manzoor8972@gmail.com>
2020-10-06 08:53:57 +0530

    Improve UX

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+function truncate(text) {
+  let temp = text;
+  if (text.length > 120) {
+    temp =
+      text.substr(0, 70) +
+      "... " +
+      text.substr(text.length - 60, text.length - 1);
+  }
+
+  return temp;
+}
 function Card({ data = "" }) {
   let styleText = "";
   if (data.length > 20 && data.length < 50) {
@@ -21,7 +32,7 @@ function Card({ data = "" }) {
           <p class="sims-p-none">434343</p>
         </div>
       </div>
-      <div class={"content " + styleText}>{data}</div>
+      <div class={"content " + styleText}>{truncate(data)}</div>
       <div class="footer">
         <div class="bookmark">
           <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEX///+m14Wk1oKn14b1+/K74KPT6sSj1oD3+/St2o+r2Yvv+Ory+e76/fjd79Kh1X03qQzxAAACa0lEQVR4nO3a3XLaMBSFUULaEgpp3v9tG6MkBeIfSdjWcWftew9ecz64Yrer3Ouvdfen9kWr9+Ow6t5+ri98flpze0JCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQsLyNz0e9/+1cP/75fRcQ9yK8B24252eKogbEe6PL92j5wriNoSXC3aruOImhF/AGuIWhB+JphWHugHhDbCcGF94lWhaYajhhd+ApcTowrtE04pCDS7suWC3kivGFg4Ai4ihhb2JpuWHGlk4AiwgBhYOJpqWG2pc4QQwmxhWOJpoWl6oUYUZwExiUOFkomk5ocYUZgKziCGFWYmmTYcaUVgAzLhiQGF2omlTxHjCogt2mwg1nLAYOEWMJixMNG001GDCKuA4MZawItG0kVBDCauBY8RIwspE0wZDDSR8CDhMjCN8ING0gVDDCB+8YLf+K0YRzgAcIAYRPpxoWl+oMYQzAXuJIYSzJJr2PdQIwhmBPcQAwtkSTbsPtb1w1gt2u/tTSnPh7MB7YmvhzImm3YTaWLgI8JbYVrhAomlXv6hNhYsBr4kthQslmvYVakPhosB/xHbCBRNN+wi1mXBx4CexlXDhRNMuoTYSrnDBbt0V2whXAl6ITYSHNRJNOz8dGghX+Q5+7tzihmslmnZaX/i6KvD982of/AucgGsetEXrqAAAAABJRU5ErkJggg==" />{" "}
